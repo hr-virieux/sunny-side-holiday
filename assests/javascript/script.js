7aa71f3fdf29c872a8828c94a5998035c44cb6c3
Faiza1st <faixa.faisal@gmail.com>
2024-01-16 19:30:14 +1100

    Comments on the Javascrpit

---
+++ assests/javascript/script.js
@@ -0,0 +1,30 @@
+// Fetch  flight data from the API 
+    // input - Api data
+    // output - data for our function FlightData / HTML(Form)
+
+//event listener for submit button
+    //input - Click from the user
+    //output - call the next function down 
+
+// Funtion for the flight data and fetch informaion and display information in HTML
+    //input - user input (departure and arrival airport / flight number) (dates) 
+    //output - The data information fligth information, status  (HTML Display as outputs) 
+ 
+
+// Fetch  Weather data from the API 
+    //input - user input (arrival airport / flight number) (dates) 
+    //output - The data information Current weather. (Display HTML as output)
+
+
+// Function for forcast 
+    //input -  Current Day Weather AKA The data information Current weather.
+    //output - Future 5 days  forcast 
+
+
+// Function storage previous Destination 
+    //input -  the users input 
+    //output - Saves it in Local Storage 
+
+// Function display previous Destination 
+    //input -  Local Storage Pull 
+    //output - Users previous input of the (departure and arrival airport / flight number) (Display)
